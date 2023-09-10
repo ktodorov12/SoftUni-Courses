b7314903dc26c0682059bc73a0597b800cac7f1b
ktodorov12 <144600066+ktodorov12@users.noreply.github.com>
2023-09-10 15:38:08 +0300

    Update login.js

--- Fundamentals/login.js
+++ Fundamentals/login.js
@@ -2,9 +2,7 @@ function login(username) {
   let trueUsername = username.shift();
   let guesses = 0;
   let password = trueUsername.split("").reverse().join("");
-  // kod za poluchavane na username i password;
   for (let check of username) {
-    // zapochvame da tursim vutre bez username dali ima suotvetstvie s parolata
     if (check !== password) {
       if (guesses >= 3) {
         console.log(`User ${trueUsername} blocked!`);
