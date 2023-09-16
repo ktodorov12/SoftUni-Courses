b470c197ae2971cd9e1e6a4b8c03df10747cb4ea
ktodorov12 <k_todorov12@abv.bg>
2023-09-16 19:49:53 +0300

    task

---
+++ Fundamentals/Data types and variables/0005 binary.js
@@ -0,0 +1,12 @@
+function binary(number){
+    let result = 0;
+    let power = number.length
+    for (let sign of number){
+        sign = Number(sign);
+        result += sign * Math.pow(2, power)
+        power--
+    }
+    console.log(result / 2)
+}
+
+binary("11110000")
