a87a5851c02124849888180c84eb8638ee9b5f6e
ktodorov12 <k_todorov12@abv.bg>
2023-09-28 20:09:23 +0300

    task

--- Fundamentals/Arrays/reverse array.js
+++ Fundamentals/Arrays/reverse array.js
@@ -1,3 +1,16 @@
-const reversing = (n, array) => console.log(reversed = array.slice(0, n).reverse().join(' '))
+const reversing = (n, array) =>
+  console.log((reversed = array.slice(0, n).reverse().join(" ")));
 
-reversing(3, [10, 20, 30, 40, 50])
+reversing(3, [10, 20, 30, 40, 50]);
+
+// longer solution;
+function revTwo(n, array) {
+  let empty = [];
+  for (let j = n - 1; j >= 0; j--) {
+    empty.push(array[j]);
+  }
+  let joined = empty.join(" ");
+  console.log(joined);
+}
+revTwo(3, [10, 20, 30, 40, 50]);
+revTwo(4, [-1, 20, 99, 5]);
