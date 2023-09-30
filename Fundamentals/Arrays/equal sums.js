58e7bb7013181cafdeadc5d0313b40f14d72fc41
ktodorov12 <k_todorov12@abv.bg>
2023-09-30 11:56:08 +0300

    task

---
+++ Fundamentals/Arrays/equal sums.js
@@ -0,0 +1,37 @@
+function equal(numbers) {
+  let numbersLength = numbers.length;
+  let isEqual = false;
+  let index = 0;
+
+  for (let i = 0; i < numbersLength; i++) {
+    let leftSum = 0;
+    let rightSum = 0;
+    if (i === 0) {
+      leftSum = 0;
+    } else if (i === numbers.length - 1) {
+      rightSum = 0;
+    }
+    for (let j = 0; j < numbersLength; j++) {
+      let currentNum = numbers[j];
+      if (i > j) {
+        leftSum += currentNum;
+      } else if (j > i) {
+        rightSum += currentNum;
+      }
+    }
+    if (leftSum === rightSum) {
+      isEqual = true;
+      index = i;
+      break;
+    } else {
+      isEqual = false;
+    }
+  }
+  if (isEqual) {
+    console.log(index);
+  } else {
+    console.log("no");
+  }
+}
+
+equal([1, 2, 3, 3]);
