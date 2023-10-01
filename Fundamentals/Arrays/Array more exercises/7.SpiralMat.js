71bc0110ff551a5d3d5fffce6b27bce24b955dbd
ktodorov12 <k_todorov12@abv.bg>
2023-10-01 17:57:41 +0300

    task

---
+++ Fundamentals/Arrays/Array more exercises/7.SpiralMat.js
@@ -0,0 +1,53 @@
+function matrix(n) {
+  let total = n * n;
+  let matrixArray = [];
+
+  for (let i = 0; i < n; i++) {
+    let zeroes = [];
+    for (let j = 0; j < n; j++) {
+      zeroes.push(0);
+    }
+    matrixArray.push(zeroes);
+  }
+
+  let rows = 0;
+  let columns = 0;
+  let step = 0;
+  for (let i = 0; i < total; ) {
+    while (columns + step < n) {
+      i++;
+      matrixArray[rows][columns] = i;
+      columns++;
+    }
+    columns--;
+    rows++;
+
+    while (rows + step < n) {
+      i++;
+      matrixArray[rows][columns] = i;
+      rows++;
+    }
+    rows--;
+    columns--;
+
+    while (columns >= step) {
+      i++;
+      matrixArray[rows][columns] = i;
+      columns--;
+    }
+    columns++;
+    rows--;
+    step++;
+
+    while (rows >= step) {
+      i++;
+      matrixArray[rows][columns] = i;
+      rows--;
+    }
+    rows++;
+    columns++;
+  }
+  matrixArray.forEach((row) => console.log(row.join(" ")));
+}
+
+matrix(3, 3);
