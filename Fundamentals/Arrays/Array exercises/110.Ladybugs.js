83f426d5286671e675cdd6555a47d74c01a0f172
ktodorov12 <k_todorov12@abv.bg>
2023-10-01 13:04:40 +0300

    modifications

--- Fundamentals/Arrays/Array exercises/110.Ladybugs.js
+++ Fundamentals/Arrays/Array exercises/110.Ladybugs.js
@@ -1,67 +1,63 @@
 function ladybugs(array) {
   let fieldSize = array[0];
   let field = [];
-  let ladybug = "ladybug";
-  let empty = "empty";
   let positions = array[1].split(" ");
-
+  //filling aray
   for (let p = 0; p < fieldSize; p++) {
-    field.push(empty);
-    for (let j = 0; j <= p; j++) {
+    field.push(0);
+    for (let j = 0; j < fieldSize; j++) {
       if (p == positions[j]) {
-        field[p] = ladybug;
+        field[p] = 1;
       }
     }
   }
 
   for (let i = 2; i < array.length; i++) {
-    let combined = array[i].split(" ");
-    let index = Number(combined[0]);
-    let command = combined[1];
-    let flyight = Number(combined[2]);
-    let traveledDistance;
+    let [index, command, flyight] = array[i].split(' ')
+    index = Number(index);
+    flyight = Number(flyight);
 
-    if (command == "right") {
-      if (field[index] == ladybug) {
-        field[index] = empty;
-        traveledDistance = field[index + flyight];
-
-        while (traveledDistance == ladybug) {
-          flyight++;
-          traveledDistance = field[index + flyight];
-        }
-
-        if (index + flyight >= field.length) {
-        } else {
-          field[flyight] = ladybug;
-        }
+    if (field[index] !== 1 || index < 0 || index >= field.length) {
+      continue;
+    }
+    //negative step
+    if (flyight < 0) {
+      flyight = flyight * -1;
+      if (command == "right") {
+        command = "left";
+      } else {
+        command = "right";
       }
+    }
+    // removing taken index
+    if (field[index] === 1) {
+      field[index] = 0;
+    }
+    //command right
+    if (command === "right") {
+      let traveledDistance = index + flyight;
+
+      if (field[traveledDistance] === 1) {
+        traveledDistance = traveledDistance + flyight;
+      }
+
+      if (traveledDistance < field.length) {
+        field[traveledDistance] = 1;
+      }
+      //command left
     } else if (command == "left") {
-      if (field[index] == ladybug) {
-        field[index] = empty;
-        traveledDistance = field[index - flyight];
+      let traveledDistance = index - flyight;
 
-        while (traveledDistance == ladybug) {
-          flyight++;
-          traveledDistance = field[index - flyight];
-        }
+      if (field[traveledDistance] === 1) {
+        traveledDistance = traveledDistance - flyight;
+      }
 
-        if (index - flyight <= field.length) {
-        } else {
-          field[flyight] = ladybug;
-        }
+      if (traveledDistance >= 0) {
+        field[traveledDistance] = 1;
       }
     }
   }
-  let str = "";
-  for (let x = 0; x < field.length; x++) {
-    if (field[x] === ladybug) {
-      str += "1 ";
-    } else if (field[x] === empty) {
-      str += "0 ";
-    }
-  }
-  console.log(str);
-}
 
-ladybugs([ 3, '0 1','0 right 1','2 right 1' ]);
+  console.log(field.join(" "));
+}
+ladybugs([3, "0 1", "0 right 1", "2 right 1"]);
