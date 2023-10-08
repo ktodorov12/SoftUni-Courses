569dcb75819e05fb926c12cb78b4c5fd2e26e3e9
ktodorov12 <k_todorov12@abv.bg>
2023-10-08 17:59:44 +0300

    task

---
+++ Fundamentals/Functions/Functions exercises/1.smallesOfThree.js
@@ -0,0 +1,1 @@
+let smallest = (one, two, three) => Math.min(one, three, two);
--- Fundamentals/Functions/Functions lab/6.signCheck.js
+++ Fundamentals/Functions/Functions lab/6.signCheck.js
@@ -5,3 +5,19 @@ function signCheck(numOne, numTwo, numThree) {
 }
 
 signCheck(-6, -12, 14);
+
+//without multiplying 
+
+function signCheck(numOne, numTwo, numThree) {
+    let negativeCount = 0;
+    let isNegative = Math.sign(numOne) === -1 ? negativeCount++ : false
+    isNegative = Math.sign(numTwo) === -1 ? negativeCount++ : false
+    isNegative = Math.sign(numThree) === -1 ? negativeCount++ : false
+    
+    if (negativeCount % 2 !== 0){
+        console.log("Negative");
+    } else {
+        console.log("Positive");
+    }
+}
+  signCheck(-5, 12, -15);
