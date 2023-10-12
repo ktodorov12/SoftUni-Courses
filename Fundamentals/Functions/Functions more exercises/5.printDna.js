c34a174a569f6f933ba4b1f1e4fac9f9f233024a
ktodorov12 <k_todorov12@abv.bg>
2023-10-12 22:07:28 +0300

    task

---
+++ Fundamentals/Functions/Functions more exercises/5.printDna.js
@@ -0,0 +1,21 @@
+function printDna(repeats) {
+  let str = "ATCGTTAGGG";
+
+  let counter = 0;
+
+  for (let i = 0; i < repeats; i++) {
+    if (i % 4 == 0) {
+      console.log(`**${str[counter % 10]}${str[(counter % 10) + 1]}**`);
+    } else if (i % 4 == 1) {
+      console.log(`*${str[counter % 10]}--${str[(counter % 10) + 1]}*`);
+    } else if (i % 4 == 2) {
+      console.log(`${str[counter % 10]}----${str[(counter % 10) + 1]}`);
+    } else if (i % 4 == 3) {
+      console.log(`*${str[counter % 10]}--${str[(counter % 10) + 1]}*`);
+    }
+
+    counter += 2;
+  }
+}
+
+printDna(10);
