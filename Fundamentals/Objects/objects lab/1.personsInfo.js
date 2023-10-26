92d1774fb22b126d98e70fdf5bc7dce823d11172
ktodorov12 <k_todorov12@abv.bg>
2023-10-26 22:18:54 +0300

    task

---
+++ Fundamentals/Objects/objects lab/1.personsInfo.js
@@ -0,0 +1,11 @@
+function personsInfo(firstName, lastName, age) {
+  let obj = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+  };
+
+  return obj;
+}
+
+personsInfo("Peter", "Pan", "20");
