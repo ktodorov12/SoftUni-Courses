563971fb121a8f7cdd2812a5371edcae3b4f0c0f
ktodorov12 <k_todorov12@abv.bg>
2023-10-27 11:26:25 +0300

    Create 6.songs.js

---
+++ Fundamentals/Objects/objects lab/6.songs.js
@@ -0,0 +1,33 @@
+function songs(input) {
+  class Song {
+    constructor(typeList, name, time) {
+      this.typeList = typeList;
+      this.name = name;
+      this.time = time;
+    }
+  }
+
+  input.shift();
+  let listType = input.pop();
+
+  let list = input.map((song) => {
+    let [type, name, time] = song.split("_");
+    return new Song(type, name, time);
+  });
+
+  let fav = list.filter((i) => i.typeList === listType);
+
+  if (listType === "all") {
+    list.forEach((song) => console.log(song.name));
+  } else {
+    fav.forEach((song) => console.log(song.name));
+  }
+}
+
+songs([
+  3,
+  "favourite_DownTown_3:14",
+  "favurite_Kiss_4:16",
+  "favourite_Smooth Criminal_4:01",
+  "favourite",
+]);
