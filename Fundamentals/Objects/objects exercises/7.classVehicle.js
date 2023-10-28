e94250cfffa774a6c1315d985becc3d5dd14f602
ktodorov12 <k_todorov12@abv.bg>
2023-10-28 20:03:54 +0300

    Create 7.classVehicle.js

---
+++ Fundamentals/Objects/objects exercises/7.classVehicle.js
@@ -0,0 +1,19 @@
+class Vehicle {
+  constructor(type, model, parts, fuel) {
+    this.type = type;
+    this.model = model;
+    this.parts = parts
+    this.parts.quality = parts.engine * parts.power
+    this.fuel = fuel;
+  }
+
+  drive(loss) {
+    this.fuel -= loss;
+  }
+}
+
+
+let parts = {engine: 9, power: 500};
+let vehicle = new Vehicle('l', 'k', parts, 840);
+vehicle.drive(20);
+console.log(vehicle.fuel);
