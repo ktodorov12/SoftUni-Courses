ac67d42847da251937735af6a496237cc48a1532
ktodorov12 <k_todorov12@abv.bg>
2023-10-28 19:25:49 +0300

    Create 5.inventory.js

---
+++ Fundamentals/Objects/objects exercises/5.inventory.js
@@ -0,0 +1,27 @@
+function inventory(info) {
+  let heroObject = [];
+
+  for (let data of info) {
+    let [hero, level, items] = data.split(" / ");
+    let obj = {
+      hero: hero,
+      level: Number(level),
+      items: items,
+    };
+    heroObject.push(obj);
+  }
+
+  let sorted = heroObject.sort((a, b) => a.level - b.level);
+
+  for (let hero of heroObject) {
+    console.log(`Hero: ${hero.hero}`);
+    console.log(`level => ${hero.level}`);
+    console.log(`Items => ${hero.items}`);
+  }
+}
+
+inventory([
+  "Isacc / 25 / Apple, GravityGun",
+  "Derek / 12 / BarrelVest, DestructionSword",
+  "Hes / 1 / Desolator, Sentinel, Antara",
+]);
