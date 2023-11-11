c480f2339651b720e6b92737aa7b1541c6f5de38
ktodorov12 <k_todorov12@abv.bg>
2023-11-11 17:00:11 +0200

    Create 8.hardWord.js

---
+++ Fundamentals/Text proccesing/Text proccesing exercises/8.hardWord.js
@@ -0,0 +1,16 @@
+function hardWord(info) {
+  let letter = info[0];
+  let missingWords = info[1].sort((a, b) => b.length - a.length);
+
+  for (let word of missingWords) {
+    let blanks = "_".repeat(word.length);
+    letter = letter.replace(blanks, word);
+  }
+
+  console.log(letter);
+}
+
+hardWord([
+  "Hi, grandma! I'm so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother's ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.",
+  ["pie", "bring", "glad", "During", "amazing", "pharmacist", "sprained"],
+]);
