9ab61d0596063f00650dda071c596fa9ac1c0492
ktodorov12 <k_todorov12@abv.bg>
2024-01-16 21:31:42 +0200

    Create 12. orbit.js

---
+++ Advanced/Advanced/Arrays and Nested Arrays/Exercises/12. orbit.js
@@ -0,0 +1,32 @@
+function solve(coordinates) {
+  let space = coordinates.slice(0, 2);
+  let [starX, starY] = coordinates.slice(2);
+
+  let matrix = [];
+
+  for (let i = 0; i < space[0]; i++) {
+    let row = [];
+    for (let j = 0; j < space[0]; j++) {
+      row.push(0);
+    }
+    matrix.push(row);
+  }
+
+  matrix[starX][starY] = 1;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (i == starX && starY == j) continue;
+      let num = 1;
+
+      if (starY !== j) num += starY + j;
+      if (starX !== i) num += starX + i;
+
+      matrix[i][j] = num;
+    }
+  }
+
+  matrix.forEach((el) => console.log(el.join(` `)));
+}
+
+solve([4, 4, 0, 0]);
