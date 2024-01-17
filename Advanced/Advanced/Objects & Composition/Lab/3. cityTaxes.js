7b67f54d2d68bb4c233378ce793c5b3a0f13cc81
ktodorov12 <k_todorov12@abv.bg>
2024-01-17 21:57:33 +0200

    Create 3. cityTaxes.js

---
+++ Advanced/Advanced/Objects & Composition/Lab/3. cityTaxes.js
@@ -0,0 +1,25 @@
+function cityTaxes(name, population, treasury) {
+  let obj = {
+    name,
+    population: Number(population),
+    treasury: Number(treasury),
+    taxRate: 10,
+    collectTaxes() {
+      this.treasury += this.population * this.taxRate;
+    },
+    applyGrowth(percentage) {
+      this.population += this.population * percentage / 100;
+    },
+    applyRecession(percentage) {
+      this.treasury -= this.treasury * percentage / 100;
+    },
+  };
+
+  return obj;
+}
+
+const city = cityTaxes('Tortuga', 7000, 15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
