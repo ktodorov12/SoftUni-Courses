33218b74c76b5563e441361e319b11b57c2dcfb4
ktodorov12 <k_todorov12@abv.bg>
2024-01-19 12:09:30 +0200

    Update 10. heroes.js

--- Advanced/Advanced/Objects & Composition/Exercises/10. heroes.js
+++ Advanced/Advanced/Objects & Composition/Exercises/10. heroes.js
@@ -1,32 +1,39 @@
-function heroes() {
-  let result = {
-    mage(name) {
-      let state = {
-        name: name,
-        health: 100,
-        mana: 100,
-        cast(spell) {
-          this.mana--;
-          console.log(`${this.name} cast ${spell}`);
-        },
-      };
-      return state;
+//object composition:
+function solve() {
+  const canCast = (state) => ({
+    cast: (spell) => {
+      console.log(`${state.name} casts ${spell}!`);
+      state.mana--;
     },
-    fighter(name) {
-      let state = {
-        name: name,
-        health: 100,
-        stamina: 100,
-        fight() {
-          this.stamina--;
-          console.log(`${this.name} slashes foe`);
-        },
-      };
-      return state;
+  });
+
+  const canFight = (state) => ({
+    fight: () => {
+      console.log(`${state.name} slashes at the foe!`);
+      state.stamina--;
     },
+  });
+
+  const fighter = (name) => {
+    let state = {
+      name,
+      health: 100,
+      stamina: 100,
+    };
+
+    return Object.assign(state, canFight(state));
   };
 
-  return result;
+  const mage = (name) => {
+    let state = {
+      name,
+      health: 100,
+      mana: 100,
+    };
+
+    return Object.assign(state, canCast(state));
+  };
+  return { mage: mage, fighter: fighter };
 }
 
 let create = heroes();
@@ -40,3 +47,35 @@ const scorcher2 = create.fighter("Scorcher 2");
 scorcher2.fight();
 console.log(scorcher2.stamina);
 console.log(scorcher.mana);
+
+// function heroes() {
+//   let result = {
+//     mage(name) {
+//       let state = {
+//         name: name,
+//         health: 100,
+//         mana: 100,
+//         cast(spell) {
+//           this.mana--;
+//           console.log(`${this.name} cast ${spell}`);
+//         },
+//       };
+//       return state;
+//     },
+//     fighter(name) {
+//       let state = {
+//         name: name,
+//         health: 100,
+//         stamina: 100,
+//         fight() {
+//           this.stamina--;
+//           console.log(`${this.name} slashes foe`);
+//         },
+//       };
+//       return state;
+//     },
+//   };
+
+//   return result;
+// }
+
