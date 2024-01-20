020484c4c10d26ded9c51ac92ddc10c3e8bb18c3
ktodorov12 <k_todorov12@abv.bg>
2024-01-20 19:45:44 +0200

    tasks

---
+++ Advanced/Advanced/DOM Introduction/Lab/04.JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+  let shown = document.getElementById("more");
+  let hidden = document.getElementById("text");
+
+  hidden.style.display = "inline";
+  shown.style.display = "none";
+}
