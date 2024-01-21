cbd1f426af81c1b38be5448ebab78f5160701a66
ktodorov12 <k_todorov12@abv.bg>
2024-01-21 20:33:31 +0200

    format text

---
+++ Advanced/Advanced/DOM Introduction/Exercises/06. Format the Text/solution.js
@@ -0,0 +1,15 @@
+function solve() {
+  let textArea = document.getElementById("input");
+  const input = textArea.value;
+  const output = document.getElementById("output");
+
+  const text = input.split(".").filter((el) => el.length > 0);
+
+  while (text.length > 0) {
+    let elements = text.splice(0, 3);
+    output.innerHTML = `<p>${
+      elements.join(". ") + "."
+    }</p>`;
+  }
+  console.log(text);
+}
