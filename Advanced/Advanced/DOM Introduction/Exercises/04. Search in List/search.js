3b6169eed9726755ef437296425ab6cf30ebc17a
ktodorov12 <k_todorov12@abv.bg>
2024-01-21 18:57:06 +0200

    Update search.js

--- Advanced/Advanced/DOM Introduction/Exercises/04. Search in List/search.js
+++ Advanced/Advanced/DOM Introduction/Exercises/04. Search in List/search.js
@@ -1,18 +1,26 @@
 function search() {
-   let allTowns = document.querySelectorAll("ul#towns li");
-   let inputedText = document.getElementById("searchText").value;
-   
-   let arrayTowns = Array.from(allTowns);
-   let matches = 0;
+  let allTowns = document.querySelectorAll("ul#towns li");
+  let inputedText = document.getElementById("searchText").value;
+  let arrayTowns = Array.from(allTowns);
 
-   for (let el of arrayTowns) {
-      let town = el.textContent
+  arrayTowns.forEach((el) => {
+    el.style.fontWeight = "normal";
+    el.style.textDecoration = "none";
+  });
+
+  let matches = 0;
+
+  if (inputedText) {
+    for (let el of arrayTowns) {
+      let town = el.textContent;
       if (town.includes(inputedText)) {
-         el.style.fontWeight = "bolder";
-         el.style.textDecoration = "underline";
-         matches++;
+        el.style.fontWeight = "bolder";
+        el.style.textDecoration = "underline";
+        matches++;
       }
-   }
+    }
+  }
 
-   document.getElementById("result").textContent = `${matches} matches found`;
+  document.getElementById("result").textContent = `${matches} matches found`;
+  document.getElementById("searchText").value = "";
 }
