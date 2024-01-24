26e8ba3c5cf38c50ffaafeca1a4fe18012fa26d3
ktodorov12 <k_todorov12@abv.bg>
2024-01-24 23:07:34 +0200

    Update

--- Advanced/Advanced/DOM Introduction/Exercises/09. Number Convertor/solution.js
+++ Advanced/Advanced/DOM Introduction/Exercises/09. Number Convertor/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+  const selection = document.getElementById("selectMenuTo");
+  createAndAppendEl("binary", "Binary");
+  createAndAppendEl("hexadecimal", "Hexadecimal");
 
-    //TODO...
+  function createAndAppendEl(value, content) {
+    let option = document.createElement("option");
+    option.textContent = content;
+    option.value = value;
+    selection.appendChild(option);
+  }
+
+  const button = document.querySelector("#container button");
+  button.addEventListener("click", onClick);
+
+  function onClick() {
+    const convertingValue = selection.value;
+    const number = Number(document.getElementById("input").value);
+
+    let result = 0;
+
+    if (convertingValue === "binary") result = number.toString(2);
+    else if (convertingValue === "hexadecimal") result = number.toString(16).toUpperCase();
+
+    document.getElementById("result").value = result;
+  }
 }
