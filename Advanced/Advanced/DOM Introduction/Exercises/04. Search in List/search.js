15ada9309b2340548842994509132a378087df5b
ktodorov12 <k_todorov12@abv.bg>
2024-01-24 22:21:02 +0200

    Update

--- Advanced/Advanced/DOM Introduction/Exercises/04. Search in List/search.js
+++ Advanced/Advanced/DOM Introduction/Exercises/04. Search in List/search.js
@@ -1,25 +1,21 @@
 function search() {
-  let allTowns = document.querySelectorAll("ul#towns li");
+  let allTowns = Array.from(document.querySelectorAll("ul#towns li"));
   let inputedText = document.getElementById("searchText").value;
-  let arrayTowns = Array.from(allTowns);
-
-  arrayTowns.forEach((el) => {
-    el.style.fontWeight = "normal";
-    el.style.textDecoration = "none";
-  });
-
+  
   let matches = 0;
-
-  if (inputedText) {
-    for (let el of arrayTowns) {
-      let town = el.textContent;
-      if (town.includes(inputedText)) {
-        el.style.fontWeight = "bolder";
-        el.style.textDecoration = "underline";
-        matches++;
-      }
+  
+  allTowns.forEach(element => {
+    let town = element.textContent
+    
+    if (town.includes(inputedText) && inputedText) {
+      element.style.fontWeight = "bolder";
+      element.style.textDecoration = "underline";
+      matches++
+    } else {
+      element.style.fontWeight = "normal";
+      element.style.textDecoration = "none";
     }
-  }
+  })
 
   document.getElementById("result").textContent = `${matches} matches found`;
   document.getElementById("searchText").value = "";
