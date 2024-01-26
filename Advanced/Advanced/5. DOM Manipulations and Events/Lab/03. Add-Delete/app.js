61780d3808239e80d6fd737e73b1852007d0a8f5
ktodorov12 <k_todorov12@abv.bg>
2024-01-26 17:40:40 +0200

    Solve Add-Delete

--- Advanced/Advanced/5. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
+++ Advanced/Advanced/5. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+  const inputRef = document.getElementById("newItemText");
+  const ulRef = document.getElementById("items");
+
+  let newLiElement = document.createElement("li");
+  newLiElement.textContent = inputRef.value;
+  ulRef.appendChild(newLiElement);
+
+  let deleteElement = document.createElement("a");
+  deleteElement.textContent = "[Delete]";
+  deleteElement.href = "#";
+  deleteElement.addEventListener("click", removeItem);
+  newLiElement.appendChild(deleteElement);
+
+  inputRef.value = "";
+
+  function removeItem() {
+    newLiElement.remove();
+  }
 }
