1bb6034d170d61c40f6d4e5e9a2941289353afcf
ktodorov12 <k_todorov12@abv.bg>
2024-01-26 17:26:24 +0200

    Solving Delete from Table

--- Advanced/Advanced/5. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
+++ Advanced/Advanced/5. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const rowsTable = Array.from(document.querySelectorAll("tbody tr"));
+  const inputRef = document.querySelector('input[type="text"]');
+  const resultatDiv = document.getElementById("result");
+  resultatDiv.textContent = "Not found.";
+
+  rowsTable.forEach((row) => {
+    let [name, email] = row.children;
+
+    if (email.textContent === inputRef.value) {
+      row.parentNode.removeChild(row);
+      resultatDiv.textContent = "Deleted.";
+    }
+  });
+
+  inputRef.value = "";
 }
