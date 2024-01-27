1159332155524303bc4e052395c09d993f6c4bd0
ktodorov12 <k_todorov12@abv.bg>
2024-01-27 13:39:48 +0200

    Solution Shopping Cart

--- Advanced/Advanced/5. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
+++ Advanced/Advanced/5. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-   //TODO...
+  const addButton = Array.from(document.querySelectorAll(".add-product"));
+  const output = document.querySelector("textarea");
+  const checkoutButton = document.querySelector(".checkout");
+
+  let total = 0;
+  let list = new Set();
+
+  addButton.forEach((button) => button.addEventListener("click", onClick));
+  checkoutButton.addEventListener("click", checkout);
+
+  function onClick() {
+    let parent = this.parentElement.parentElement;
+    const productName = parent.querySelector(".product-title").textContent;
+    const productPrice = parent.querySelector(".product-line-price").textContent;
+
+    total += Number(productPrice);
+    list.add(productName);
+
+    output.value += `Added ${productName} for ${productPrice} to the cart.\n`;
+  }
+
+  function checkout() {
+    output.value += `You bought ${Array.from(list).join(", ")} for ${total.toFixed(2)}.`;
+    addButton.forEach((button) => button.removeEventListener("click", onClick));
+    checkoutButton.removeEventListener("click", checkout)
+  }
 }
