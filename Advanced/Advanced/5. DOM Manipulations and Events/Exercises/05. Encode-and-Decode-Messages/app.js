cc6216c149279ed5f2a16d8ed12dbc09a21434ee
ktodorov12 <k_todorov12@abv.bg>
2024-01-27 20:43:12 +0200

    Update app.js

--- Advanced/Advanced/5. DOM Manipulations and Events/Exercises/05. Encode-and-Decode-Messages/app.js
+++ Advanced/Advanced/5. DOM Manipulations and Events/Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,38 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const messageText = document.querySelector("#main div:nth-child(1)");
+  const senderTextarea = messageText.querySelector("textarea");
+  const button = messageText.querySelector("button");
+
+  const recieverRef = document.querySelector("#main div:nth-child(2)");
+  const recieverTextarea = recieverRef.querySelector("textarea");
+  const recieverButton = recieverRef.querySelector("button");
+
+  button.addEventListener("click", decodeMessage);
+  recieverButton.addEventListener("click", encodeMessage);
+
+  let encodedText = "";
+  let decodedText = "";
+
+  function decodeMessage() {
+    let text = senderTextarea.value;
+
+    for (let letter of text) {
+      let number = letter.charCodeAt();
+      let encodedLetter = String.fromCharCode(number + 1);
+      encodedText += encodedLetter;
+    }
+    recieverTextarea.value = encodedText;
+    senderTextarea.value = "";
+  }
+
+  function encodeMessage() {
+    for (let letter of encodedText) {
+      let number = letter.charCodeAt();
+      decodedText += String.fromCharCode(number - 1);
+    }
+    recieverTextarea.value = "";
+    recieverTextarea.value = decodedText;
+  }
 }
+
+//The password for my bankaccount is 123password321
