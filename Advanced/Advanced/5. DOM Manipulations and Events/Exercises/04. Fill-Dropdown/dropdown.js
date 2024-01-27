069951aa72859b710c835ffd045b93f8fbd82209
ktodorov12 <k_todorov12@abv.bg>
2024-01-27 20:10:04 +0200

    Solution dropdown menu

--- Advanced/Advanced/5. DOM Manipulations and Events/Exercises/04. Fill-Dropdown/dropdown.js
+++ Advanced/Advanced/5. DOM Manipulations and Events/Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+  const inputText = document.getElementById("newItemText");
+  const inputValue = document.getElementById("newItemValue");
+  const menu = document.getElementById("menu");
+
+  let newOption = document.createElement("option");
+  newOption.value = inputValue.value;
+  newOption.textContent = inputText.value;
+
+  menu.appendChild(newOption);
+
+  inputText.value = "";
+  inputValue.value = "";
 }
