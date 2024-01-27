79061fa25bbb357ee80847d1727e46c7f7767391
ktodorov12 <k_todorov12@abv.bg>
2024-01-27 14:24:45 +0200

    Sollution Sections

--- Advanced/Advanced/5. DOM Manipulations and Events/Exercises/01. Sections/script.js
+++ Advanced/Advanced/5. DOM Manipulations and Events/Exercises/01. Sections/script.js
@@ -1,3 +1,28 @@
 function create(words) {
-   console.log('TODO:...');
+  const content = document.getElementById("content");
+
+  words.forEach((text) => {
+    let div = createAndAppendDiv();
+    createAndAppendPara(div, text);
+  });
+
+  function createAndAppendDiv() {
+    let newDiv = document.createElement("div");
+    newDiv.addEventListener("click", onClick);
+    content.appendChild(newDiv);
+    return newDiv;
+  }
+
+  function createAndAppendPara(apendingDiv, text) {
+    let newPara = document.createElement("p");
+    newPara.style.display = "none";
+    newPara.textContent = text;
+    apendingDiv.appendChild(newPara);
+
+   }
+
+   function onClick(event) {
+     event.currentTarget.children[0].style.display = "block";
+   }
+
 }
