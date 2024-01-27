f3caed6a46ed72b61ef87697fd12146a9e1a17a9
ktodorov12 <k_todorov12@abv.bg>
2024-01-27 10:37:36 +0200

    Solution Mouse Gradient

--- Advanced/Advanced/5. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
+++ Advanced/Advanced/5. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,14 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  const gradientBox = document.getElementById("gradient");
+  const result = document.getElementById("result");
+  gradientBox.addEventListener("mousemove", showPercent);
+  gradientBox.addEventListener("mouseout", () => {
+    result.textContent = "Mouse Out!";
+  });
+
+  function showPercent(event) {
+    let width = event.target.clientWidth;
+    let percent = Math.trunc((event.offsetX / width) * 100);
+    result.textContent = `${percent}%`;
+  }
 }
