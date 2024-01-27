cfd3f66dd65001c9be8bc3808c477cc1b7f732e4
ktodorov12 <k_todorov12@abv.bg>
2024-01-27 19:21:58 +0200

    Solution Locked Profile

--- Advanced/Advanced/5. DOM Manipulations and Events/Exercises/03. Locked-Profile/app.js
+++ Advanced/Advanced/5. DOM Manipulations and Events/Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const buttons = Array.from(document.querySelectorAll("button"));
+
+  buttons.forEach((button) => button.addEventListener("click", showMore));
+
+  function showMore() {
+    let lockBtn = this.parentElement.querySelector('input[value="lock"]').checked;
+    const hiddenContent = this.parentElement.getElementsByTagName('div')[0];
+
+    if (lockBtn == false) {
+      if (this.textContent === "Show more") {
+        hiddenContent.style.display = "block"
+        this.textContent = "Hide it";
+      } else if (this.textContent === "Hide it"){
+        hiddenContent.style.display = "none"
+        this.textContent = "Show more";
+      }
+    }
+  }
 }
