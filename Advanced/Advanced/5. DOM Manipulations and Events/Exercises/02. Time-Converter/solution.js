c93490c1b99ab71cd235c64fb5e507b4ff1c5869
ktodorov12 <k_todorov12@abv.bg>
2024-01-27 15:28:58 +0200

    Solution time converter

--- Advanced/Advanced/5. DOM Manipulations and Events/Exercises/02. Time-Converter/solution.js
+++ Advanced/Advanced/5. DOM Manipulations and Events/Exercises/02. Time-Converter/solution.js
@@ -1,4 +1,27 @@
 function attachEventsListeners() {
+  const textFields = Array.from(document.querySelectorAll("div input:nth-child(2)"));
+  const inputField = Array.from(document.querySelectorAll('input[type="button"]'));
 
-    console.log('TODO:...');
+  inputField.forEach((button) => button.addEventListener("click", onClick));
+
+  function onClick() {
+    let text = this.previousElementSibling;
+
+    let operations = {
+      days: Number(text.value),
+      hours: Number(text.value) / 24,
+      minutes: Number(text.value) / 24 / 60,
+      seconds: Number(text.value) / 24 / 60 / 60,
+    };
+
+    let number = operations[text.id];
+    calculate(number);
+  }
+
+  function calculate(number) {
+    textFields[0].value = number;
+    textFields[1].value = number * 24;
+    textFields[2].value = number * 24 * 60;
+    textFields[3].value = number * 24 * 60 * 60;
+  }
 }
