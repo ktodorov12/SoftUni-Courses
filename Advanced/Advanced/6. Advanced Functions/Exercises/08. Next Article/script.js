1b75ebdc127f97f82a5ed3b8a1a74c81e4b969d8
ktodorov12 <k_todorov12@abv.bg>
2024-02-03 19:04:01 +0200

    Update script.js

--- Advanced/Advanced/6. Advanced Functions/Exercises/08. Next Article/script.js
+++ Advanced/Advanced/6. Advanced Functions/Exercises/08. Next Article/script.js
@@ -1,3 +1,16 @@
 function getArticleGenerator(articles) {
-    // TODO
+  let arts = Array.from(articles);
+  const content = document.getElementById("content");
+
+  function createEl() {
+    if (arts[0] === undefined) {
+      return;
+    } else {
+      let firstEl = arts.shift();
+      let newArticle = document.createElement("article");
+      newArticle.textContent = firstEl;
+      content.appendChild(newArticle);
+    }
+  }
+  return createEl;
 }
