b57e3d1d8ce7f3abfdd0a9a10dd677375a4f2e50
ktodorov12 <k_todorov12@abv.bg>
2024-02-03 19:03:58 +0200

    Update app.js

--- Advanced/Advanced/6. Advanced Functions/Exercises/07. Simple Calculator/app.js
+++ Advanced/Advanced/6. Advanced Functions/Exercises/07. Simple Calculator/app.js
@@ -1,7 +1,27 @@
 function calculator() {
-    // TODO:
+  let firstNumRef;
+  let secondNumRef;
+  let resultRef;
+
+  function init(selec1, selec2, selecResult) {
+    firstNumRef = document.querySelector(selec1);
+    secondNumRef = document.querySelector(selec2);
+    resultRef = document.querySelector(selecResult);
+  }
+
+  function add() {
+    resultRef.value = Number(firstNumRef.value) + Number(secondNumRef.value);
+  }
+  function subtract() {
+    resultRef.value = Number(firstNumRef.value) - Number(secondNumRef.value);
+  }
+
+  return {
+    init,
+    add,
+    subtract,
+  };
 }
 
-
-
-
+const calculate = calculator();
+calculate.init("#num1", "#num2", "#result");
