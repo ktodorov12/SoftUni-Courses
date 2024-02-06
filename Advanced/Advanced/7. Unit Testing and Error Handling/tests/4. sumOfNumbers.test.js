548df853d917ba07000268dffb835d7f5f8e103d
ktodorov12 <k_todorov12@abv.bg>
2024-02-06 22:02:32 +0200

    Update 4. sumOfNumbers.test.js

--- Advanced/Advanced/7. Unit Testing and Error Handling/tests/4. sumOfNumbers.test.js
+++ Advanced/Advanced/7. Unit Testing and Error Handling/tests/4. sumOfNumbers.test.js
@@ -5,9 +5,6 @@ describe("Test one", () => {
   it("given input is array", () => {
     expect(typeof sum([1, 2, 3])).to.equal("number");
     expect(sum([1, 2, 3])).to.equal(6);
-  });
-
-  it("s", () => {
-    expect(Array.isArray(arr)).to.equal("array");
+    expect(sum([1])).to.equal(1);
   });
 });
