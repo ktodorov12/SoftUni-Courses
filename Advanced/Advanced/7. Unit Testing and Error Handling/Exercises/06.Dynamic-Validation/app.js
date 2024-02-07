b21bcd08e7063005a5c09b89853ff60379a51d74
ktodorov12 <k_todorov12@abv.bg>
2024-02-07 23:16:46 +0200

    Update app.js

--- Advanced/Advanced/7. Unit Testing and Error Handling/Exercises/06.Dynamic-Validation/app.js
+++ Advanced/Advanced/7. Unit Testing and Error Handling/Exercises/06.Dynamic-Validation/app.js
@@ -1,3 +1,10 @@
 function validate() {
-    // TODO
+  const input = document.getElementById("email");
+  input.addEventListener("change", onChange);
+  const pattern = /[a-z]+@[a-z]+.[a-z]+/gm;
+
+  function onChange() {
+    if (pattern.test(input.value)) input.classList.remove("error");
+    else input.classList.add("error");
+  }
 }
