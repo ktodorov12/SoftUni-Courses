eb32584c1f8ba5f2a8de4da65af291ac04d925e1
ktodorov12 <k_todorov12@abv.bg>
2024-02-11 20:02:02 +0200

    Create 7. hex.js

---
+++ Advanced/Advanced/8. Classes/Exercises/7. hex.js
@@ -0,0 +1,39 @@
+class Hex {
+  value;
+
+  constructor(value) {
+    this.value = Number(value);
+  }
+
+  valueOf() {
+    return this.value;
+  }
+
+  toString() {
+    return "0x" + this.value.toString(16).toUpperCase();
+  }
+
+  plus(value) {
+    return new Hex(this.value + Number(value));
+  }
+
+  minus(value) {
+    return new Hex(this.value - Number(value));
+  }
+
+  parse(string) {
+    return parseInt(string, 16);
+  }
+}
+
+let FF = new Hex(255);
+
+console.log(FF.toString());
+
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString() === "0xF");
+console.log(FF.parse("AAA"));
