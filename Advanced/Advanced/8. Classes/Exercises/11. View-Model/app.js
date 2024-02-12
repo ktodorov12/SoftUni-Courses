751a29b3ece311e72ad6c0b7a16ff26d4091509e
ktodorov12 <k_todorov12@abv.bg>
2024-02-12 16:17:52 +0200

    Update app.js

--- Advanced/Advanced/8. Classes/Exercises/11. View-Model/app.js
+++ Advanced/Advanced/8. Classes/Exercises/11. View-Model/app.js
@@ -1,8 +1,48 @@
 class Textbox {
-    //TODO
+  selector;
+  _invalidSymbols;
+
+  constructor(selector, pattern) {
+    this.selector = selector;
+    this._invalidSymbols = pattern;
+    this._elements = document.querySelectorAll(this.selector);
+  }
+
+  get value() {
+    return this._elements[0].value;
+  }
+
+  set value(input) {
+    Array.from(this._elements).map((el) => {
+      el.value = input;
+    });
+  }
+
+  get elements() {
+    return this._elements;
+  }
+
+  isValid() {
+    let elements = Array.from(this._elements);
+
+    for (let el of elements) {
+      let value = el.value;
+
+      if (value.match(this._invalidSymbols)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
-let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
-let inputs = document.getElementsByClassName('.textbox');
+let textbox = new Textbox(".textbox", /[^a-zA-Z0-9]/);
+textbox.elements[0].value = "g";
+let inputs = document.querySelectorAll(".textbox");
 
-inputs.addEventListener('click',function(){console.log(textbox.value);});
+inputs.forEach((el) =>
+  el.addEventListener("click", function () {
+    console.log(textbox.value = "Jhon");
+  })
+);
