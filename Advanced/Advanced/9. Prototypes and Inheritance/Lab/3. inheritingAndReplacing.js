b32223b6227d05bb85404ebd8e5d21379f007025
ktodorov12 <k_todorov12@abv.bg>
2024-02-13 16:26:15 +0200

    Create 3. inheritingAndReplacing.js

---
+++ Advanced/Advanced/9. Prototypes and Inheritance/Lab/3. inheritingAndReplacing.js
@@ -0,0 +1,45 @@
+function solve() {
+  class Person {
+    name;
+    email;
+
+    constructor(name, email) {
+      this.name = name;
+      this.email = email;
+    }
+
+    toString() {
+        return `Person (name: ${this.name}, email: ${this.email})`;
+      }
+  }
+
+  class Teacher extends Person {
+    constructor(name, email, subject) {
+      super(name, email);
+      this.subject = subject;
+    }
+
+    toString() {
+      return `Teacher (name: ${this.name}, email: ${this.email}, subject: ${this.subject})`;
+    }
+  }
+
+  class Studnet extends Person {
+    constructor(name, email, course) {
+      super(name, email);
+      this.course = course;
+    }
+
+    toString() {
+      return `Student (name: ${this.name}, email: ${this.email}, course: ${this.course})`;
+    }
+  }
+
+  return {
+    Person,
+    Studnet,
+    Teacher,
+  };
+}
+
+solve()
