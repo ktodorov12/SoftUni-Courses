5f164df49af65b94a34642fa23fb4d4da98236b4
ktodorov12 <k_todorov12@abv.bg>
2024-02-24 13:03:02 +0200

    Update app.js

--- Advanced/Applications/02.JS-Applications-Asynchronous-Programming-Lab-Resources/02.Github-Repos/scripts/app.js
+++ Advanced/Applications/02.JS-Applications-Asynchronous-Programming-Lab-Resources/02.Github-Repos/scripts/app.js
@@ -1,3 +1,34 @@
+const output = document.getElementById("repos");
+
 function loadRepos() {
-	console.log("TODO...");
+  const username = document.getElementById("username").value;
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  fetch(url).then(onHeaders).then(onSuccess).catch(onError);
+}
+function createLiItem({ html_url, full_name }) {
+  const item = document.createElement("li");
+
+  const anchor = document.createElement("a");
+  anchor.href = html_url;
+  anchor.textContent = full_name;
+  item.appendChild(anchor);
+
+  return item;
+}
+
+function onHeaders(response) {
+  if (!response.ok) {
+    throw "Error";
+  }
+
+  return response.json();
+}
+
+function onSuccess(data) {
+  output.replaceChildren(...data.map(createLiItem));
+}
+
+function onError(error) {
+  output.textContent = error;
 }
