1acb3aa91611d3f17bf5ca593e023ece166d9171
ktodorov12 <k_todorov12@abv.bg>
2024-02-24 13:34:15 +0200

    Update app.js

--- Advanced/Applications/02.JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
+++ Advanced/Applications/02.JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
@@ -1,4 +1,35 @@
+const commits = document.getElementById("commits");
+
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+  const username = document.getElementById("username").value;
+  const repo = document.getElementById("repo").value;
+  const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+  
+  fetch(url).then(onHandlers).then(onSuccess).catch(onError);
+}
+
+function onHandlers(response) {
+  if (!response.ok) {
+    throw new Error();
+  }
+
+  return response.json();
+}
+
+function onSuccess(data) {
+  commits.replaceChildren(...data.map(createLi));
+}
+
+function onError(error) {
+  commits.textContent = error
+}
+
+function createLi({ commit }) {
+  const name = commit.author.name;
+  const message = commit.message;
+
+  const li = document.createElement("li");
+  li.textContent = `${name}: ${message}`;
+
+  return li;
 }
