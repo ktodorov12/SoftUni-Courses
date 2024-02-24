d61413b11307d7e7a04bfe33cce76059fccf11f5
ktodorov12 <k_todorov12@abv.bg>
2024-02-24 14:03:46 +0200

    Update app.js

--- Advanced/Applications/02.JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
+++ Advanced/Applications/02.JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
@@ -1,27 +1,59 @@
+// solution fetch/then
+// const commits = document.getElementById("commits");
+
+// function loadCommits() {
+//   const username = document.getElementById("username").value;
+//   const repo = document.getElementById("repo").value;
+//   const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+//   fetch(url).then(onHandlers).then(onSuccess).catch(onError);
+// }
+
+// function onHandlers(response) {
+//   if (!response.ok) {
+//     throw new Error();
+//   }
+
+//   return response.json();
+// }
+
+// function onSuccess(data) {
+//   commits.replaceChildren(...data.map(createLi));
+// }
+
+// function onError(error) {
+//   commits.textContent = error
+// }
+
+// function createLi({ commit }) {
+//   const name = commit.author.name;
+//   const message = commit.message;
+
+//   const li = document.createElement("li");
+//   li.textContent = `${name}: ${message}`;
+
+//   return li;
+// }
+
+//solution async/await
 const commits = document.getElementById("commits");
 
-function loadCommits() {
+async function loadCommits() {
   const username = document.getElementById("username").value;
   const repo = document.getElementById("repo").value;
   const url = `https://api.github.com/repos/${username}/${repo}/commits`;
-  
-  fetch(url).then(onHandlers).then(onSuccess).catch(onError);
-}
 
-function onHandlers(response) {
-  if (!response.ok) {
-    throw new Error();
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    const data = await response.json();
+    commits.replaceChildren(...data.map(createLi));
+  } catch (error) {
+    commits.textContent = error;
   }
-
-  return response.json();
-}
-
-function onSuccess(data) {
-  commits.replaceChildren(...data.map(createLi));
-}
-
-function onError(error) {
-  commits.textContent = error
 }
 
 function createLi({ commit }) {
