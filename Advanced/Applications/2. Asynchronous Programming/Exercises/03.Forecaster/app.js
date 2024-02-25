9459ffb30424be871afbf73c1f14be886a89cb62
ktodorov12 <k_todorov12@abv.bg>
2024-02-25 13:46:44 +0200

    Solution Forecast Data

--- Advanced/Applications/2. Asynchronous Programming/Exercises/03.Forecaster/app.js
+++ Advanced/Applications/2. Asynchronous Programming/Exercises/03.Forecaster/app.js
@@ -7,7 +7,6 @@ function attachEvents() {
 
 async function onClick() {
   const inputField = document.getElementById("location").value;
-
   const url = "http://localhost:3030/jsonstore/forecaster/locations";
 
   try {
@@ -15,11 +14,26 @@ async function onClick() {
     const data = await response.json();
     const found = data.find((obj) => obj.name == inputField);
 
+    if (!found || !response.ok || !response.status) {
+      throw new Error("Location not found");
+    }
+
     currentWeatherCondition(found.code);
     threeDayForecast(found.code);
     document.getElementById("forecast").style.display = "block";
-  } catch (err) {}
+  } catch (err) {
+    document.getElementById("forecast").textContent = "Error";
+    document.getElementById("forecast").style.display = "block";
+  }
 }
+
+const symb = {
+  Sunny: "\u2600",
+  "Partly sunny": "\u26C5",
+  Overcast: "\u2601",
+  Rain: "\u2614",
+};
+const degr = String.fromCharCode(176);
 
 async function currentWeatherCondition(code) {
   const currentDiv = document.getElementById("current");
@@ -28,39 +42,47 @@ async function currentWeatherCondition(code) {
   const response = await fetch(url);
   const { name, forecast } = await response.json();
 
-  const symb = {
-    Sunny: /&#x2600/,
-    "Partly sunny": /&#x26C5/,
-    Overcast: /&#x2601/,
-    Rain: /&#x2614/,
-  };
-  const degr = /&#176/;
-
   const mainDiv = createEl("div", "forecasts");
-  const spanWeatherSym = createEl(
-    "span",
-    "condition symbol",
-    symb[forecast.condition]
-  );
+  const spanWeatherSym = createEl("span", "condition symbol", symb[forecast.condition]);
 
   const spanCondition = createEl("span", "condition");
   const spanCreator = (cont) => createEl("span", "forecast-data", cont);
   const spanCity = spanCreator(name);
-  const spanDegrees = spanCreator(
-    `${forecast.low}${degr}/${forecast.high}${degr}`
-  );
+  const spanDegrees = spanCreator(`${forecast.low}${degr}/${forecast.high}${degr}`);
   const spanWeather = spanCreator(forecast.condition);
 
   appender(spanCondition, spanCity, spanDegrees, spanWeather);
   appender(mainDiv, spanWeatherSym, spanCondition);
-  currentDiv.appendChild(mainDiv);
+  currentDiv.replaceChildren(mainDiv);
+
+  const labelDiv = createEl("div", "label", "Current conditions");
+  currentDiv.prepend(labelDiv);
 }
 
 async function threeDayForecast(code) {
+  const uppcomingDiv = document.getElementById("upcoming");
   const url = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`;
 
   const response = await fetch(url);
   const { name, forecast } = await response.json();
+  
+  const [dayOne, dayTwo, dayThree] = forecast.map(spanCreator)
+  const mainDiv = createEl("div", "forecast-info");
+  appender(mainDiv, dayOne, dayTwo, dayThree)
+  uppcomingDiv.replaceChildren(mainDiv);
+
+  const labelDiv = createEl("div", "label", "Three-day forecast");
+  uppcomingDiv.prepend(labelDiv);
+}
+
+function spanCreatorThreeDay(weather) {
+  const mainSpan = createEl("span", "upcoming");
+  const symbSpan = createEl("span", "symbol", symb[weather.condition]);
+  const dgrSpan = createEl("span", "forecast-data", `${weather.low}${degr}/${weather.high}${degr}`)
+  const weatherSpan = createEl("span", "forecast-data", weather.condition);
+
+  appender(mainSpan, symbSpan, dgrSpan, weatherSpan);
+  return mainSpan
 }
 
 function createEl(type, className, content) {
