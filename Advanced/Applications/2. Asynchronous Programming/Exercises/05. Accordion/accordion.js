96bf2dab657579fab6a0dea249b4a3914f60ed64
ktodorov12 <k_todorov12@abv.bg>
2024-02-25 18:48:28 +0200

    Solution Accordion

--- Advanced/Applications/2. Asynchronous Programming/Exercises/05. Accordion/accordion.js
+++ Advanced/Applications/2. Asynchronous Programming/Exercises/05. Accordion/accordion.js
@@ -1,3 +1,64 @@
-function solution() {
-    //TODO .....
+window.addEventListener("load", solution);
+
+async function solution() {
+  const url = "http://localhost:3030/jsonstore/advanced/articles/list";
+  const response = await fetch(url);
+  const dataOfBoxes = await response.json();
+  printBoxes(dataOfBoxes);
+}
+
+async function printBoxes(dataOfBoxes) {
+  const divs = await Promise.all(
+    Object.values(dataOfBoxes).map(async (boxData) => {
+      const { _id } = boxData;
+      const urlAdditionalData = `http://localhost:3030/jsonstore/advanced/articles/details/${_id}`;
+      const receivedResponse = await fetch(urlAdditionalData);
+      const contentOfBox = await receivedResponse.json();
+      return createBox(contentOfBox);
+    })
+  );
+
+  const main = document.getElementById("main");
+  main.replaceChildren(...divs);
+}
+
+function createBox(contentOfBox) {
+  const { title, content, _id } = contentOfBox;
+
+  const mainDiv = createEl("div", "", { class: "accordion" });
+
+  const divHead = createEl("div", "", { class: "head" });
+  const spanName = createEl("span", title);
+  const bttn = createEl("button", "More", { class: "button", id: _id });
+  bttn.addEventListener("click", showMore);
+  appender(divHead, spanName, bttn);
+
+  const divExtra = createEl("div", "", { class: "extra" });
+  const para = createEl("p", content);
+  divExtra.appendChild(para);
+
+  appender(mainDiv, divHead, divExtra);
+  return mainDiv;
+}
+
+function showMore(e) {
+  const buttonState = e.target;
+  const hiddenContent = e.target.parentElement.parentElement.querySelector(".extra");
+
+  hiddenContent.style.display = buttonState.textContent === "More" ? "block" : "none";
+  buttonState.textContent = hiddenContent.style.display === "none" ? "More" : "Less";
+}
+
+function createEl(type, content, attributes = {}) {
+  const el = document.createElement(type);
+
+  for (let key in attributes) {
+    el.setAttribute(key, attributes[key]);
+  }
+  el.textContent = content;
+  return el;
+}
+
+function appender(parent, ...children) {
+  children.forEach((child) => parent.appendChild(child));
 }
