de53d9e9b8be0b41e16e02434dbf90cc1d72e7b7
ktodorov12 <k_todorov12@abv.bg>
2024-03-03 18:24:01 +0200

    Update Messenger task

--- Advanced/Applications/3. Remote data and Authentication/Exercises/01.Messenger/app.js
+++ Advanced/Applications/3. Remote data and Authentication/Exercises/01.Messenger/app.js
@@ -1,5 +1,59 @@
-function attachEvents() {
-    console.log('TODO...');
-}
+const url = "http://localhost:3030/jsonstore/messenger";
+const textArea = document.getElementById("messages");
 
 attachEvents();
+function attachEvents() {
+  const sendBtn = document.getElementById("submit");
+  const refreshBtn = document.getElementById("refresh");
+
+  sendBtn.addEventListener("click", sendInfo);
+  refreshBtn.addEventListener("click", refreshInfro);
+}
+
+async function sendInfo() {
+  const authorRef = document.querySelector("input[name='author']");
+  const messageRef = document.querySelector("input[name='content']");
+
+  if (!authorRef.value || !messageRef.value) {
+    return;
+  }
+  const body = JSON.stringify({
+    author: authorRef.value,
+    content: messageRef.value,
+  });
+
+  try {
+    const response = await fetch(url, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body,
+    });
+
+    if (!response.ok) {
+      const err = await response.json();
+      throw new Error(err);
+    }
+
+    authorRef.value = "";
+    messageRef.value = "";
+    refreshInfro();
+  } catch (error) {
+    alert(error?.message);
+  }
+}
+
+async function refreshInfro() {
+  try {
+    textArea.textContent = "";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const res = Object.values(data).reduce(
+      (r, { author, content }) => r + `${author}: ${content}\n`,
+      ""
+    );
+    textArea.textContent = res.trim();
+  } catch (err) {
+    alert(err);
+  }
+}
