b4c511eb45aa76630bf98cf36eb30b8c92f0ca85
ktodorov12 <k_todorov12@abv.bg>
2024-03-03 18:24:21 +0200

    Solution Phonebook task

--- Advanced/Applications/3. Remote data and Authentication/Exercises/02.Phonebook/app.js
+++ Advanced/Applications/3. Remote data and Authentication/Exercises/02.Phonebook/app.js
@@ -1,5 +1,61 @@
-function attachEvents() {
-    console.log('TODO...');
-}
+const url = "http://localhost:3030/jsonstore/phonebook";
+const phonebook = document.getElementById("phonebook");
+const personRef = document.getElementById("person");
+const phoneRef = document.getElementById("phone");
 
 attachEvents();
+function attachEvents() {
+  document.getElementById("btnLoad").addEventListener("click", loadContacts);
+  document.getElementById("btnCreate").addEventListener("click", createContact);
+}
+
+async function createContact() {
+  try {
+    await fetch(url, {
+      method: "post",
+      headers: { "Content-Type": "application/js" },
+      body: JSON.stringify({ person: personRef.value, phone: phoneRef.value }),
+    });
+
+    personRef.value = "";
+    phoneRef.value = "";
+    loadContacts();
+  } catch (err) {
+    alert(err);
+  }
+}
+
+async function loadContacts() {
+  phonebook.innerHTML = "";
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    Object.values(data).map(createLi);
+  } catch (error) {}
+}
+
+async function deleteContact(event) {
+  const elId = event.target.parentElement.dataset.id;
+  try {
+    await fetch(url + "/" + elId, {
+      method: "delete",
+    });
+    loadContacts();
+  } catch (error) {
+    alert(err);
+  }
+}
+
+function createLi(data) {
+  const { phone, person, _id } = data;
+
+  const li = document.createElement("li");
+  li.textContent = `${person}: ${phone}`;
+
+  const btn = document.createElement("button");
+  btn.textContent = "Delete";
+  btn.addEventListener("click", deleteContact);
+  li.appendChild(btn);
+  phonebook.appendChild(li);
+  li.dataset.id = _id;
+}
