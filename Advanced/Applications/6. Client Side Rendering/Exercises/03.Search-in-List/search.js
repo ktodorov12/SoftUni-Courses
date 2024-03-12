beca5f0ef6d98c3639f06b76cd7477129fe768a4
ktodorov12 <k_todorov12@abv.bg>
2024-03-12 16:35:11 +0200

    Update Search-list

--- Advanced/Applications/6. Client Side Rendering/Exercises/03.Search-in-List/search.js
+++ Advanced/Applications/6. Client Side Rendering/Exercises/03.Search-in-List/search.js
@@ -1,3 +1,27 @@
-function search() {
-   // TODO
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import { towns } from "./towns.js";
+
+const inputBox = document.getElementById("searchText");
+const result = document.getElementById("result");
+const root = document.getElementById("towns");
+render(renderTowns(), root);
+
+document.querySelector("button").addEventListener("click", () => {
+  render(renderTowns(), root);
+  inputBox.value = "";
+  const found = document.querySelectorAll(".active");
+  result.textContent = `${found.length} matches found`;
+});
+
+function renderTowns() {
+  return html` 
+  <ul>
+    ${towns.map(townTemplate)}
+  </ul>`;
+}
+
+function townTemplate(town) {
+  const val = inputBox.value;
+  const isActive = town.includes(val) && val != "";
+  return html`<li class=${isActive ? "active" : ""}>${town}</li>`;
 }
