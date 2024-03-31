5354ce8cd6c840c3369e7870ee925d57c9772164
ktodorov12 <k_todorov12@abv.bg>
2024-03-31 20:08:43 +0300

    Creating Edit View

--- Advanced/Applications/8. Modular Applications/Exercise/01.Team-Manager/src/views/editView.js
+++ Advanced/Applications/8. Modular Applications/Exercise/01.Team-Manager/src/views/editView.js
@@ -1,24 +1,53 @@
-import { editTeam } from "../data/teams.js";
+import { editTeam, getDetailsTeam } from "../data/teams.js";
+import { renderError } from "../data/users.js";
 import { html, render, page } from "../lib.js";
 import { createSubmitHandler } from "../util.js";
 
-//TODO add html template
-function editTemplate() {
-  return html``;
+function editTemplate(hasError, team) {
+  return html` 
+  <section id="edit">
+    <article class="narrow">
+      <header class="pad-med">
+        <h1>Edit Team</h1>
+      </header>
+      <form @submit=${createSubmitHandler(team.onEdit)} id="edit-form" class="main-form pad-large">
+        ${hasError
+          ? html`<div class="error">${hasError.message}</div>`
+          : null}
+        <label>Team name: <input type="text" name="name" .value=${team.name} /></label>
+        <label>Logo URL: <input type="text" name="logoUrl" .value=${team.logoUrl} /></label>
+        <label>Description: <textarea name="description" .value=${team.description}></textarea></label>
+        <input class="action cta" type="submit" value="Save Changes" />
+      </form>
+    </article>
+  </section>`;
 }
 
-//TODO add logic
 export async function showEditView(ctx) {
   const id = ctx.params.id;
+  const teamDetails = await getDetailsTeam(id);
+  teamDetails.onEdit = onEdit
+  render(editTemplate(null, teamDetails));
 
-  //TODO check this function
   async function onEdit(data) {
     const check = Object.values(data).some((x) => x == "");
     if (check) {
-      return alert("All fields requiered");
+      return renderError(editTemplate, "All fields requiered", teamDetails);
     }
 
-    await editTeam(id, data);
-    page.redirect(`/details/${id}`);
+    if (data.name.length < 4) {
+      return renderError(editTemplate, "Name is too short", teamDetails);
+    }
+  
+    if (data.description.length < 10) {
+      return renderError(editTemplate, "Description is too short", teamDetails);
+    }
+  
+    try {
+      await editTeam(id, data);
+      page.redirect(`/details/${id}`);
+    } catch (error) {
+      renderError(editTemplate, error.message, teamDetails);
+    }
   }
 }
