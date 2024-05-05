da870b1877eaaa33a861a0ce8c57f7bf062c49db
ktodorov12 <k_todorov12@abv.bg>
2024-05-05 21:07:01 +0300

    add cat handler to the main index handler so it is exported

--- JS Back-End/Cat Shelter/handlers/index.js
+++ JS Back-End/Cat Shelter/handlers/index.js
@@ -1,4 +1,5 @@
 const homeHandler = require("./home");
 const staticFilesHandler = require("./static-handler");
+const catHandler = require("./cat");
 
-module.exports = [homeHandler, staticFilesHandler];
+module.exports = [homeHandler, staticFilesHandler, catHandler];
