a80e9a343426bd2d7e7d4a1f99e68273c0011c08
ktodorov12 <k_todorov12@abv.bg>
2024-05-05 21:35:30 +0300

    Remove handle check becouse of the asyncronous execute of the handlers

--- JS Back-End/Cat Shelter/index.js
+++ JS Back-End/Cat Shelter/index.js
@@ -3,18 +3,11 @@ const port = 5000;
 const handlers = require("./handlers");
 
 const server = http.createServer((req, res) => {
-  let handled = false;
   
   for (let handler of handlers) {
     if (!handler(req, res)) {
-      handled = true;
       break;
     }
-  }
-
-  if (!handled) {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found");
   }
 });
 
