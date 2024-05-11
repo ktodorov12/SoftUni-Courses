d90d160380bd8165f47b5e692ab6168fd239caf7
ktodorov12 <k_todorov12@abv.bg>
2024-05-11 20:46:39 +0300

    refactor the code to follow the DRY principle to avoide code duplication

--- JS Back-End/Cat Shelter/handlers/delete-edit.js
+++ JS Back-End/Cat Shelter/handlers/delete-edit.js
@@ -6,22 +6,14 @@ const formTemplate = require("../views/templates/newHomeTemp.js");
 
 module.exports = async (req, res) => {
   const [pathname, catId] = req.url.split("?");
+  const filePath = path.join(__dirname, "../views", "catShelter.html");
+  const catsPath = path.join(__dirname, "../data", "cats.json");
 
   if (pathname === "/cats-find-new-home" && req.method === "GET") {
-    const filePath = path.join(__dirname, "../views", "catShelter.html");
-    const catsPath = path.join(__dirname, "../data", "cats.json");
-
     try {
-      const allCatData = JSON.parse(await fsPromises.readFile(catsPath, { encoding: "utf-8" }));
-      const addoptedCat = allCatData.find((cat) => cat.id == catId);
+      const { foundCat } = await findNeededCat();
 
-      if (!addoptedCat) {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end("404 Cat not found");
-        return;
-      }
-
-      const formView = formTemplate(addoptedCat);
+      const formView = formTemplate(foundCat);
       const shelterView = await fsPromises.readFile(filePath, { encoding: "utf-8" });
 
       res.writeHead(200, { "Content-Type": "text/html" });
@@ -35,20 +27,10 @@ module.exports = async (req, res) => {
 
     res.end();
   } else if (pathname === "/cats-find-new-home" && req.method === "POST") {
-    const filePath = path.join(__dirname, "../views", "catShelter.html");
-    const catsPath = path.join(__dirname, "../data", "cats.json");
-
     try {
-      const allCatData = JSON.parse(await fsPromises.readFile(catsPath, { encoding: "utf-8" }));
-      const addoptedCat = allCatData.find((cat) => cat.id == catId);
+      const { allCatData, foundCat } = await findNeededCat();
 
-      if (!addoptedCat) {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end("404 Cat not found");
-        return;
-      }
-
-      const index = allCatData.indexOf(addoptedCat);
+      const index = allCatData.indexOf(foundCat);
       allCatData.splice(index, 1);
       await fsPromises.writeFile(catsPath, JSON.stringify(allCatData, null, 2));
 
@@ -63,5 +45,25 @@ module.exports = async (req, res) => {
     res.end();
   } else {
     return true;
+  }
+
+  async function findNeededCat() {
+    try {
+      const allCatData = JSON.parse(await fsPromises.readFile(catsPath, { encoding: "utf-8" }));
+      const foundCat = allCatData.find((cat) => cat.id == catId);
+
+      if (!foundCat) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Cat not found");
+        return;
+      }
+
+      return { allCatData, foundCat };
+    } catch (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("500 Internal Server Error");
+      console.error(err); // Log the error for debugging purposes
+      return;
+    }
   }
 };
