2f32803e7336ee6f7e3bfabb59f871f1cd79bdc0
ktodorov12 <k_todorov12@abv.bg>
2024-05-11 20:20:11 +0300

    create back-end logic for sheltering new cat, hence - find and delete the cat from the data with cats and redirect to the main page

--- JS Back-End/Cat Shelter/handlers/delete-edit.js
+++ JS Back-End/Cat Shelter/handlers/delete-edit.js
@@ -26,11 +26,41 @@ module.exports = async (req, res) => {
 
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(shelterView.toString().replace("{{catForm}}", formView));
-      res.end();
     } catch (err) {
-      res.end(err);
-      throw new Error(err);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("500 Internal Server Error");
+      console.error(err); // Log the error for debugging purposes
+      return;
     }
+
+    res.end();
+  } else if (pathname === "/cats-find-new-home" && req.method === "POST") {
+    const filePath = path.join(__dirname, "../views", "catShelter.html");
+    const catsPath = path.join(__dirname, "../data", "cats.json");
+
+    try {
+      const allCatData = JSON.parse(await fsPromises.readFile(catsPath, { encoding: "utf-8" }));
+      const addoptedCat = allCatData.find((cat) => cat.id == catId);
+
+      if (!addoptedCat) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Cat not found");
+        return;
+      }
+
+      const index = allCatData.indexOf(addoptedCat);
+      allCatData.splice(index, 1);
+      await fsPromises.writeFile(catsPath, JSON.stringify(allCatData, null, 2));
+
+      res.writeHead(301, { location: "/" });
+    } catch (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("500 Internal Server Error");
+      console.error(err); // Log the error for debugging purposes
+      return;
+    }
+
+    res.end();
   } else {
     return true;
   }
