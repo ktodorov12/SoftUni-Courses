a9630a393308a2285e9e9e814b3980be1a4251ab
ktodorov12 <k_todorov12@abv.bg>
2024-05-11 20:06:17 +0300

    Create new template for the sheltering of cat

---
+++ JS Back-End/Cat Shelter/views/templates/newHomeTemp.js
@@ -0,0 +1,18 @@
+const catFindNewHomeTemp = (cat) => {
+  return `        
+    <form action="/cats-find-new-home?${cat.id}" method="POST" class="cat-form">
+        <h2>Shelter the cat</h2>
+        <img src="${cat.imageUrl}" alt="">
+        <label for="name">Name</label>
+        <input type="text" id="name" value="${cat.name}" disabled>
+        <label for="description">Description</label>
+        <textarea id="description" disabled>${cat.description}</textarea>
+        <label for="group">Breed</label>
+        <select id="group" disabled>
+            <option value="${cat.breed}">Fluffy Cat</option>
+        </select>
+        <button>SHELTER THE CAT</button>
+    </form>`;
+};
+
+module.exports = catFindNewHomeTemp;
