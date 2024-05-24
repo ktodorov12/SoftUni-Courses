f2f4e87f925d24f4605f20595cc3eeaebd8e18b3
ktodorov12 <k_todorov12@abv.bg>
2024-05-24 21:49:59 +0300

    Set up express-hendlebars engine

--- JS Back-End/Movie Magic/src/index.js
+++ JS Back-End/Movie Magic/src/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const hbs = require("express-handlebars");
+const handlebars = require("express-handlebars");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+const hbs = handlebars.create({ extname: ".hbs" });
+
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", ".hbs");
+app.use(".hbs", hbs.engine);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
