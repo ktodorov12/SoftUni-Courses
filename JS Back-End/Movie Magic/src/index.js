c1dac802ce36f14f3804f8e7f6f299060aa76a72
ktodorov12 <k_todorov12@abv.bg>
2024-05-25 11:22:50 +0300

    Create separate configuration for HBS

--- JS Back-End/Movie Magic/src/index.js
+++ JS Back-End/Movie Magic/src/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const handlebars = require("express-handlebars");
 const { home } = require("./controllers/catalog");
+const { hbsConfig } = require("./config/hbs");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const hbs = handlebars.create({ extname: ".hbs" });
-
+hbsConfig(app);
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", ".hbs");
-app.engine(".hbs", hbs.engine);
 
 app.use("/static", express.static("static"));
 
