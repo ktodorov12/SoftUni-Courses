12ccbe4c5d72ee7fb2e0e15df1ee9558a1380d43
ktodorov12 <k_todorov12@abv.bg>
2024-06-01 20:32:58 +0300

    Rename movie service

--- JS Back-End/Movie Magic/src/services/movies.js
+++ JS Back-End/Movie Magic/src/services/movies.js
@@ -1,45 +1,14 @@
-const fs = require("fs/promises");
-const path = require("path");
 const Movie = require("../models/Movie");
 
-const dataPath = path.join(__dirname, "..", "..", "data", "database.json");
-
-async function readDatabase() {
-  const movies = JSON.parse(await fs.readFile(dataPath));
-  return movies;
-}
-
-async function writeDatabase(data) {
-  const movies = JSON.parse(await fs.readFile(dataPath));
-  movies.push(data);
-  await fs.writeFile(dataPath, JSON.stringify(movies, null, 2));
-}
-
-function movieModel(movie) {
-  return new Movie(movie);
-}
-
 module.exports = {
-  allMovies: async () => {
-    const movies = await readDatabase();
-    return movies.map(movieModel);
-  },
-  movieById: async (id) => {
-    const movies = await readDatabase();
-    const foundMovie = movies.find((m) => m.id == id);
-    return movieModel(foundMovie);
-  },
-  createMovie: async (data) => {
-    data.id = 5; // TODO: fix hardcoded id;
-    const movie = movieModel(data);
-    await writeDatabase(movie);
-    return movie;
-  },
+  allMovies: async () => await Movie.find().lean(),
+  movieById: async (id) => await Movie.findById(id).populate("cast").lean(),
+  createMovie: async (data) => await Movie.create(data),
   searchMovie: async ({ title, genre, year }) => {
-    const movies = await readDatabase();
+    const movies = await Movie.find().lean();
 
     if (!title && !genre && !year) {
-      return movies.map(movieModel);
+      return movies;
     }
 
     const found = movies.filter((m) => {
