734166cfb0ade9f6c25191f26a1402daea58a74c
ktodorov12 <k_todorov12@abv.bg>
2024-06-07 21:49:37 +0300

    Add express middlewares

--- JS Back-End/Movie Magic/src/config/express.js
+++ JS Back-End/Movie Magic/src/config/express.js
@@ -1,8 +1,18 @@
 const { urlencoded, static: staticHandler } = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 function expressConfig(app) {
   app.use(urlencoded({ extended: true }));
   app.use("/static", staticHandler("static"));
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: "keyborad cat",
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
 }
 
 module.exports = { expressConfig };
