59434e74b2c9608904455479aab5b3d4d52c1f31
ktodorov12 <k_todorov12@abv.bg>
2024-06-08 22:07:12 +0300

    Change express session middleware

--- JS Back-End/Movie Magic/src/config/express.js
+++ JS Back-End/Movie Magic/src/config/express.js
@@ -1,18 +1,12 @@
 const { urlencoded, static: staticHandler } = require("express");
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
+const { session } = require("../middlewares/session");
 
 function expressConfig(app) {
+  app.use(cookieParser());
+  app.use(session());
   app.use(urlencoded({ extended: true }));
   app.use("/static", staticHandler("static"));
-  app.use(cookieParser());
-  app.use(
-    session({
-      secret: "keyborad cat",
-      resave: false,
-      saveUninitialized: true,
-    })
-  );
 }
 
 module.exports = { expressConfig };
