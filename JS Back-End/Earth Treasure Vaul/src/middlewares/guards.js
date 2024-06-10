c1266d6f2fede586bb1b873b5e674e6b9a2fcf7f
ktodorov12 <k_todorov12@abv.bg>
2024-06-10 23:06:25 +0300

    Add new guard for owner check

--- JS Back-End/Earth Treasure Vaul/src/middlewares/guards.js
+++ JS Back-End/Earth Treasure Vaul/src/middlewares/guards.js
@@ -1,3 +1,5 @@
+const Stones = require("../models/Stones");
+
 function isGuest() {
   return function (req, res, next) {
     const user = req.user;
@@ -22,7 +24,26 @@ function isUser() {
   };
 }
 
+function isOwner() {
+  return async function (req, res, next) {
+    const { stoneId } = req.params;
+    const { userId } = req.user;
+
+    const foundStone = await Stones.findOne({
+      _id: stoneId,
+      ownerId: userId,
+    });
+
+    if (foundStone) {
+      next();
+    } else {
+      res.redirect("/");
+    }
+  };
+}
+
 module.exports = {
   isGuest,
   isUser,
+  isOwner,
 };
