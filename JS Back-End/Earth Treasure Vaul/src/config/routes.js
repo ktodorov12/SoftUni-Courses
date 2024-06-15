56688a2da2328b9bb0c584295976ad28037c6473
ktodorov12 <k_todorov12@abv.bg>
2024-06-15 11:02:36 +0300

    Change route config

--- JS Back-End/Earth Treasure Vaul/src/config/routes.js
+++ JS Back-End/Earth Treasure Vaul/src/config/routes.js
@@ -1,34 +1,17 @@
-const { Router } = require("express");
-
-const { isUser, isGuest, isOwner } = require("../middlewares/guards");
-
-const { home, dashboard, search } = require("../controllers/catalog");
-const { loginGet, loginPost, registerGet, registerPost, logout } = require("../controllers/user");
+const { catalogRouter } = require("../controllers/catalog");
+const { userRouter } = require("../controllers/user");
+const { detailsRouter } = require("../controllers/details");
+const { stoneRouter } = require("../controllers/stones");
 const { notFound } = require("../controllers/notFound");
-const { stonesGet, stonesPost, like, editGet, editPost, deleteStone } = require("../controllers/stones");
-const { details } = require("../controllers/details");
 
-const router = Router();
-router.get("/", home);
-router.route("/login")
-    .get(isUser(), loginGet)
-    .post(isUser(), loginPost);
-router.route("/register")
-    .get(isUser(), registerGet)
-    .post(isUser(), registerPost);
-router.get("/logout", isGuest(), logout);
-router.get("/dashboard", dashboard);
-router.get("/search", search);
 
-router.route("/stones")
-    .get(isGuest(), stonesGet)
-    .post(isGuest(), stonesPost);
-router.get("/details/:stoneId", details);
-router.get("/like/:stoneId", isGuest(), like);
-router.route("/edit/:stoneId")
-    .get(isGuest(), isOwner(), editGet)
-    .post(isGuest(), isOwner(), editPost);
-router.get("/delete/:stoneId", isGuest(), isOwner(), deleteStone);
+function routeConfig(app) {
+  app.use(catalogRouter);
+  app.use(userRouter);
+  app.use(detailsRouter);
+  app.use(stoneRouter);
 
-router.get("*", notFound);
-module.exports = router;
+  app.get("*", notFound);
+}
+
+module.exports = routeConfig;
