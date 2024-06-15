6b8b475c94b4612978dcd042061485c8d26a64f9
ktodorov12 <k_todorov12@abv.bg>
2024-06-15 11:02:55 +0300

    Update details intro router

--- JS Back-End/Earth Treasure Vaul/src/controllers/details.js
+++ JS Back-End/Earth Treasure Vaul/src/controllers/details.js
@@ -1,17 +1,21 @@
+const { Router } = require("express");
+
 const { getStoneById } = require("../services/stones");
 const { isOwnerOrLiked } = require("../util/helper");
 
-module.exports = {
-  details: async (req, res) => {
-    const { stoneId } = req.params;
-    const stone = await getStoneById(stoneId);
-    isOwnerOrLiked(stone, req.user.userId);
-    
-    if (!stone) {
-      res.render("404");
-      return;
-    }
+const detailsRouter = Router();
 
-    res.render("details", { stone });
-  },
-};
+detailsRouter.get("/details/:stoneId", async (req, res) => {
+  const { stoneId } = req.params;
+  const stone = await getStoneById(stoneId);
+  isOwnerOrLiked(stone, req.user?.userId);
+
+  if (!stone) {
+    res.render("404");
+    return;
+  }
+
+  res.render("details", { stone });
+});
+
+module.exports = { detailsRouter };
