a25d8017ea581f912f2b26fa820c5e048c0416cb
ktodorov12 <k_todorov12@abv.bg>
2024-06-15 11:03:19 +0300

    update user into routes and add validation

--- JS Back-End/Earth Treasure Vaul/src/controllers/user.js
+++ JS Back-End/Earth Treasure Vaul/src/controllers/user.js
@@ -1,11 +1,18 @@
+const { Router } = require("express");
+const { body, validationResult } = require('express-validator');
+
 const { createToken } = require("../services/token");
 const { login, register } = require("../services/user");
+const { isUser, isGuest } = require("../middlewares/guards");
 
-module.exports = {
-  loginGet: (req, res) => {
+const userRouter = Router();
+
+userRouter
+  .route("/login")
+  .get(isUser(), (req, res) => {
     res.render("login");
-  },
-  loginPost: async (req, res) => {
+  })
+  .post(isUser(), async (req, res) => {
     try {
       const user = await login(req.body);
       const token = createToken(user._id);
@@ -14,22 +21,43 @@ module.exports = {
     } catch (error) {
       res.render("login", { email: req.body.email });
     }
-  },
-  registerGet: (req, res) => {
+  });
+
+userRouter
+  .route("/register")
+  .get(isUser(), (req, res) => {
     res.render("register");
-  },
-  registerPost: async (req, res) => {
-    try {
-      const user = await register(req.body);
-      const token = createToken(user._id);
-      res.cookie("user", token, { httpOnly: true });
-      res.redirect("/");
-    } catch (error) {
-      res.render("register", { email: req.body.email });
+  })
+  .post(
+    isUser(),
+    body("email").trim().isEmail().withMessage("Please enter a valid email"),
+    body("password").trim().isAlphanumeric().isLength({ min: 6 }).withMessage("Password must be at least 6 characters long and may contain only English letters and numbers"),
+    body("repass")
+      .trim()
+      .custom((value, { req }) => value == req.body.password)
+      .withMessage("Passwords don't match"),
+    async (req, res) => {
+      try {
+        const result = validationResult(req);
+
+        if (result.errors.length) {
+          throw result.errors;
+        }
+
+        const user = await register(req.body);
+        const token = createToken(user._id);
+
+        res.cookie("user", token, { httpOnly: true });
+        res.redirect("/");
+      } catch (error) {
+        res.render("register", { email: req.body.email });
+      }
     }
-  },
-  logout: (req, res) => {
-    res.clearCookie("user");
-    res.redirect("/");
-  },
-};
+  );
+
+userRouter.get("/logout", isGuest(), (req, res) => {
+  res.clearCookie("user");
+  res.redirect("/");
+});
+
+module.exports = { userRouter };
