2493155e2726fb6380c8ffb2d17cda843177fca3
ktodorov12 <k_todorov12@abv.bg>
2024-07-01 21:43:41 +0300

    Create configuration for handlebars

---
+++ JS Back-End/HomeCookingRecipes/src/config/hbs.js
@@ -0,0 +1,9 @@
+const handlebars = require("express-handlebars");
+
+function hbsConfig(app) {
+  const hbs = handlebars.create({ extname: ".hbs" });
+  app.set("view engine", ".hbs");
+  app.engine(".hbs", hbs.engine);
+}
+
+module.exports = hbsConfig;
