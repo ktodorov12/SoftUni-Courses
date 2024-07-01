f17db0392a939acbf56d42ccd128671f2c621763
ktodorov12 <k_todorov12@abv.bg>
2024-07-01 21:45:40 +0300

    Create session middleware

---
+++ JS Back-End/HomeCookingRecipes/src/middlewares/session.js
@@ -0,0 +1,21 @@
+const { verifyToken } = require("../services/token");
+
+function session() {
+  return function (req, res, next) {
+    const user = req.cookies?.user;
+
+    if (user) {
+      try {
+        const payload = verifyToken(user);
+        req.user = payload;
+        res.locals.hasUser = true;
+      } catch (error) {
+        res.clearCookie("user");
+      }
+    }
+
+    next();
+  };
+}
+
+module.exports = session;
