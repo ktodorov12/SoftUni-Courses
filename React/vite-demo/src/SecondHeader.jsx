28589bc5c0f80c7bac30db6a7e957ba715c23920
ktodorov12 <k_todorov12@abv.bg>
2024-07-02 21:53:04 +0300

    create second header component

---
+++ React/vite-demo/src/SecondHeader.jsx
@@ -0,0 +1,8 @@
+export default function SecondHeader() {
+  return (
+    <h2>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic unde odit corporis natus velit eaque, nemo ex veritatis ad a facilis totam dolor obcaecati, dignissimos earum non
+      sapiente labore? Itaque!
+    </h2>
+  );
+}
