9b2620d19c4e272b130419ca14033c2b95f06c17
ktodorov12 <k_todorov12@abv.bg>
2024-07-02 21:47:18 +0300

    Set up App function

---
+++ React/component-state/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "./components/Counter";
+import Timer from "./components/Timer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Timer />
+
+      <Counter />
+    </>
+  );
+}
+
+export default App;
