db18e9e4c7d844656f5401499baf9f3bdb6a541e
ktodorov12 <k_todorov12@abv.bg>
2024-07-02 21:52:23 +0300

    Create component

---
+++ React/vite-demo/src/FunctionExpression.jsx
@@ -0,0 +1,8 @@
+export default function FunctionExpression() {
+  return (
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae provident ea odio eligendi est veritatis doloremque modi. Mollitia dignissimos beatae repellendus quaerat
+      vero, vitae qui voluptatem saepe, modi, sequi illo?
+    </p>
+  );
+}
