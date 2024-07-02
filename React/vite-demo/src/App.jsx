05b8cc7d08b61cdebf686540edff1a6b2c687518
ktodorov12 <k_todorov12@abv.bg>
2024-07-02 21:52:11 +0300

    Create App component

---
+++ React/vite-demo/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import FunctionExpression from "./FunctionExpression";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <FunctionExpression />
+    </>
+  );
+}
