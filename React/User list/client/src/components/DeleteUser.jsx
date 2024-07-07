8643b1e311b93df76216a4cd1556f63833b01d2e
ktodorov12 <k_todorov12@abv.bg>
2024-07-07 22:51:23 +0300

    Update DeleteUser.jsx

--- React/User list/client/src/components/DeleteUser.jsx
+++ React/User list/client/src/components/DeleteUser.jsx
@@ -1,13 +1,13 @@
-export default function DeleteUser() {
+export default function DeleteUser({ onDeleteUser, onClose }) {
   return (
     <>
       <div className="overlay">
-        <div className="backdrop"></div>
+        <div className="backdrop" onClick={onClose}></div>
         <div className="modal">
           <div className="confirm-container">
             <header className="headers">
               <h2>Are you sure you want to delete this account?</h2>
-              <button className="btn close">
+              <button className="btn close" onClick={onClose}>
                 <svg
                   aria-hidden="true"
                   focusable="false"
@@ -25,10 +25,10 @@ export default function DeleteUser() {
             </header>
             <div className="actions">
               <div id="form-actions">
-                <button id="action-save" className="btn" type="submit">
+                <button id="action-save" className="btn" type="submit" onClick={onDeleteUser}>
                   Delete
                 </button>
-                <button id="action-cancel" className="btn" type="button">
+                <button id="action-cancel" className="btn" type="button" onClick={onClose}>
                   Cancel
                 </button>
               </div>
