4e09998d95e45c432fdceb3c4f83cb0ec85a9d80
ktodorov12 <k_todorov12@abv.bg>
2024-07-07 22:52:20 +0300

    Add logic for details per user

--- React/User list/client/src/components/UserCardContainer.jsx
+++ React/User list/client/src/components/UserCardContainer.jsx
@@ -3,11 +3,14 @@ import Table from "./Table";
 import AddUserButton from "./AddUserButton";
 import Pagination from "./Pagination";
 import CreateEditForm from "./CreateEditForm";
+import UserDetails from "./UserDetails";
 import { useEffect, useState } from "react";
 
 export default function UserCardContainer() {
   const [users, setUsers] = useState([]);
   const [createClicked, setCreateClicked] = useState(false);
+
+  const [userDetails, setUserDetails] = useState(null);
   useEffect(() => {
     (async function () {
       setUsers(Object.values(await getAllUsers()));
@@ -29,6 +32,7 @@ export default function UserCardContainer() {
         {createClicked && <CreateEditForm onCloseCreate={() => setCreateClicked(false)} onCreateUser={handleCreate} />}
 
         <Table />
+        {userDetails && <UserDetails user={userDetails} onCloseDetails={() => setUserDetails(null)} />}
 
         <AddUserButton />
         <AddUserButton onCreateClick={() => setCreateClicked(true)} />
