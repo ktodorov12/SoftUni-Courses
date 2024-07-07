87f18a4c21634eed74c9148e4e0fb1a036cce2d5
ktodorov12 <k_todorov12@abv.bg>
2024-07-07 22:50:30 +0300

    create date parser util

---
+++ React/User list/client/src/utils/dateUtils.js
@@ -0,0 +1,6 @@
+export function formatDate(dateString) {
+  const date = new Date(dateString);
+  const dateOptions = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", dateOptions);
+  return formattedDate;
+}
