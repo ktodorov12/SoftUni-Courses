574dedb8be105ff91a1bcc440387dbb12f7be752
ktodorov12 <k_todorov12@abv.bg>
2024-07-07 22:53:08 +0300

    create logic to handle deleting users

--- React/User list/client/src/components/UserCardContainer.jsx
+++ React/User list/client/src/components/UserCardContainer.jsx
@@ -4,13 +4,17 @@ import AddUserButton from "./AddUserButton";
 import Pagination from "./Pagination";
 import CreateEditForm from "./CreateEditForm";
 import UserDetails from "./UserDetails";
+import DeleteUser from "./DeleteUser";
+
 import { useEffect, useState } from "react";
+import { createUser, deleteUser, editUser, getAllUsers } from "../data/data";
 
 export default function UserCardContainer() {
   const [users, setUsers] = useState([]);
   const [createClicked, setCreateClicked] = useState(false);
 
   const [userDetails, setUserDetails] = useState(null);
+  const [deleteUserId, setDeleteUserId] = useState(null);
   useEffect(() => {
     (async function () {
       setUsers(Object.values(await getAllUsers()));
@@ -25,6 +29,16 @@ export default function UserCardContainer() {
     setCreateClicked(false);
     setIsLoading(false);
   }
+
+  async function handleDelete(e) {
+    setIsLoading(true);
+    e.preventDefault();
+
+    await deleteUser(deleteUserId);
+    setUsers(users.filter((u) => u._id !== deleteUserId));
+    setDeleteUserId(null);
+    setIsLoading(false);
+  }
   return (
     <>
       <section className="card users-container">
@@ -35,6 +49,10 @@ export default function UserCardContainer() {
         {userDetails && <UserDetails user={userDetails} onCloseDetails={() => setUserDetails(null)} />}
 
         <AddUserButton />
+        {deleteUserId && <DeleteUser onDeleteUser={handleDelete} onClose={() => setDeleteUserId(null)} />}
+
+          users={users}
+          onDelete={(userId) => setDeleteUserId(userId)}
         <AddUserButton onCreateClick={() => setCreateClicked(true)} />
 
         <Pagination />
